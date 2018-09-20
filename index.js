4e71823306cbbc53820dd122d048ac4c8f3e0d2a
Mary Jane Phifer <mjphifer@gmail.com>
2018-09-20 23:34:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,14 +11,17 @@ function logWhisper(string) {
   console.log('hello')
 }
 function sayHiToGrandma(string) {
-  if (`string` === lowercase.toLowerCase) {
+  if (string.toLowerCase ( )=== string) {
     return "I can't hear you!"
 }
 function sayHiToGrandma(string) {
-  if (`string`=== uppercase.toUpperCase) {
+  if (string.toUpperCase () === string) {
     return "YES INDEED!"
 }
 function sayHiToGrandma(string) {
-  if (`string`=== "I love you, Grandma."){
+  if (string === "I love you, Grandma.") {
     return "I love you, too."
+}
+
+return "Are you eating enough?"
 }
