7333189d07cf51cd779701be3deab80cd531b825
Mary Jane Phifer <mjphifer@gmail.com>
2018-09-20 23:36:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 function shout(string) {
   return string.toUpperCase ()
 }
+
 function whisper(string) {
   return string.toLowerCase ()
 }
+
 function logShout(string) {
-  console.log('HELLO')
+  console.log(string.toUpperCase ())
 }
+
 function logWhisper(string) {
-  console.log('hello')
+  console.log(string.toLowerCase ())
 }
+
 function sayHiToGrandma(string) {
   if (string.toLowerCase ( )=== string) {
     return "I can't hear you!"
